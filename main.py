18891910122ceb15eb395564e48daf176a04710c
TheLionCoder <eorlando.reyesruiz@gmail.com>
2026-02-21 21:36:36 +0000

    Testing Build Trigger

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from flask import Flask, \
-    render_template, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,5 +8,4 @@ def main():
     return render_template("index.html", model=model)
 
 if __name__ == "__main__":
-app.run(host='0.0.0.0', port=8080, \
-    debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
